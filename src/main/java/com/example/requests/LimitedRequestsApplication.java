85308c13c8a8a469942d2a3b9e98b46fc54734c4
Yana Guseva <jangus@bk.ru>
2022-07-12 22:47:12 +0400

    Init application

---
+++ src/main/java/com/example/requests/LimitedRequestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.requests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitedRequestsApplication.class, args);
+	}
+
+}
